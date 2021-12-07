d0d6e2dd25e6fdf78f32b2c3b07df5ebc7dc35ca
Tahir Civan <69795597+nomadtahir@users.noreply.github.com>
2021-12-07 21:21:41 +0100

    Update SimplifyModifier.js

--- SimplifyModifier.js
+++ SimplifyModifier.js
@@ -2,8 +2,8 @@ import {
 	BufferGeometry,
 	Float32BufferAttribute,
 	Vector3
-} from '../../../build/three.module.js';
-import { BufferGeometryUtils } from '../utils/BufferGeometryUtils.js';
+} from 'three.module.js';
+import { BufferGeometryUtils } from 'BufferGeometryUtils.js';
 
 /**
  *	Simplification Geometry Modifier
